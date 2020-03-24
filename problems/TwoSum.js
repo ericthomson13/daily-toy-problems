5ba0ff7ab957d4a0dd679cdf5de4eaaa9d045787
et <eric.thomson13@gmail.com>
2020-03-24 15:28:30 -0600

    Added Solution for TwoSum

--- problems/TwoSum.js
+++ problems/TwoSum.js
@@ -6,6 +6,25 @@ https://leetcode.com/problems/two-sum/
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
 
+const twoSum = (nums, target) => {
+  // handle edge case if nums is invalid input
+  if (nums.length < 2) return false;
+  // iterate through starting from start
+  for (let i = 0; i < nums.length; i++) {
+    // iterate through starting one index after i
+    for (let j = i + 1; j < nums.length; j++) {
+      // check to see if nums add up to target
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  // return false if target not found
+  return false;
+};
 
+/* 
+time complexity could be improved by finding a single loop over the nested for loops
 */
