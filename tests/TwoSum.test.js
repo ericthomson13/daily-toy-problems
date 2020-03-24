9155840491f62c671f5b3b237ad329178a69f68e
et <eric.thomson13@gmail.com>
2020-03-24 16:45:50 -0600

    Working to Add Testing Suite

---
+++ tests/TwoSum.test.js
@@ -0,0 +1,10 @@
+const twoSum = require('../problems/TwoSum');
+
+describe('Two Sums Tests', () => {
+  it('should return the correct indices', () => {
+    expect(twoSum([1,2], 3)[0]).toBe(0);
+  });
+  xit('should return false when the inputs are invalid', () => {
+
+  });
+});
