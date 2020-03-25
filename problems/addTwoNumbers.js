48d70f5c02674e54ae308b4a9b285586de269f3b
et <eric.thomson13@gmail.com>
2020-03-25 09:21:52 -0600

    Added addTwoNumbers Solution Incomplete Working on building SLL in addTwoNumbers LeetCode didn't seem to lend itself to JavaScript for this problem very well. Building complete solution.  Will eventually move Node and Linked List to a DataStructures Folder with Other DataStructures

--- problems/addTwoNumbers.js
+++ problems/addTwoNumbers.js
@@ -13,20 +13,83 @@ Example:
   Explanation: 342 + 465 = 807.
 */
 
-/**
+/*
  * Definition for singly-linked list.
  * function ListNode(val) {
  *     this.val = val;
  *     this.next = null;
  * }
  */
-/*
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
+
+ /*
+  Example List structure: 
+  ListNode = {
+    val: 2,
+    next: ListNode { 
+      val: 4, 
+      next: ListNode { 
+        val: 3, next: null 
+      } 
+    } 
+  }
  */
+class List {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  };
+  push (val) {
+    const newNode = new Node(val);
+    if (!!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
+  pop () {
+    if (this.tail === this.head) {
+      this.tail = null;
+      this.head = null;
+      return;
+    }
+    let current = this.head;
+    let temp;
+    while (!!current.next) {
+      
+    }
+  }
+};
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+};
+
 const addTwoNumbers = (l1, l2) => {
-    
+  // helper function to convert both lists to numbers
+  const listToNumber = (list) => {
+    // start result as string so can add to front
+    let result = [];
+    // have current start at beginning of list
+    let current = list;
+    // loop through list and keep adding node to front of string
+    do {
+      // add current value to result string
+      result.unshift(current.val);
+      // move to next node
+      current = list.next;
+    } while(!!current);
+    // return result converted to number
+    return parseInt(result.join(''));
+  };
+  // convert both lists to strings of digits
+  let n1 = listToNumber(l1);
+  let n2 = listToNumber(l2);
+  // return two numbers added together
+  return n1 + n2;
 };
 
 export default addTwoNumbers;
