827ca4961fb93ccefba42b9f5f6530de4fc1201f
et <eric.thomson13@gmail.com>
2020-03-26 09:54:03 -0600

    Added HashTable

---
+++ utilityDataStructures/hashTable.js
@@ -0,0 +1,71 @@
+// using separate chaining rather than linear probing for collisions
+
+class HashTable {
+  constructor(size = 53) {
+      this.keyMap = new Array(size);
+  }
+
+  _hash (string, arrayLength) {
+    let total = 0;
+    // many use prime numbers because it decreases numbers of collisions
+      // and this helps better spread the data throughout the hashTable
+      // prime number arrayLength also can decrease collisions by many orders of magnitude
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0); - 96
+      total = (total + WEIRD_PRIME + value) % arrayLength;
+    }
+    return total;
+  }
+
+  set(key, value) {
+    let index = _hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+  }
+
+  get (key) {
+    let index = _hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index]; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  values () {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1])){
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
+
+  keys () {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0])){
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
+};
+
+export default HashTable;
