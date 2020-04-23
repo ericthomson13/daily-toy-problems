b874ab4c472521e55ca04e3cce1467e30e4e3288
et <eric.thomson13@gmail.com>
2020-04-23 09:06:07 -0600

    Bitwise And Range Solution Code

---
+++ problems/bitwiseAndOfRange.js
@@ -0,0 +1,32 @@
+/*
+
+Bitwise AND of Numbers Range
+
+Code prompt found at:
+https://leetcode.com/problems/bitwise-and-of-numbers-range/
+Difficulty: Medium
+
+Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+Example 1:
+Input: [5,7]
+Output: 4
+
+Example 2:
+Input: [0,1]
+Output: 0
+
+*/
+
+const rangeBitwiseAnd = (m, n) => {
+  let res = m;
+
+  for (let i = m + 1; i <= n; i++) {
+    res = res & i;
+    if (res === 0) return 0;
+  }
+
+  return res;
+}
+
+export default rangeBitwiseAnd;
