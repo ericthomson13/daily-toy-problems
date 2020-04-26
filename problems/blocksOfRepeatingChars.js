110e1a02548e8faeb87593c94b9a73b85075f6a4
et <eric.thomson13@gmail.com>
2020-04-26 09:39:56 -0600

    JumpGame DP and Brute Solutions

---
+++ problems/blocksOfRepeatingChars.js
@@ -0,0 +1,83 @@
+
+
+function solution(S, K) {
+  // edge cases of S being empty or K being 0
+  if (!S.length) return 0;
+  if (K === 0) return S.length;
+  // count for array of tuples
+  const count = [
+      [1, S[0]],
+  ];
+
+  // iterate through S building tuples
+  for (let i = 1; i < S.length; i++) {
+      // letter the same as last
+      if (count[count.length - 1][1] === S[i]) {
+          // add to count of tuple;
+          count[count.length - 1][0]++;
+      } else {
+          // if not the same add new tuple with count and letter
+          // was getting correct answer for ex test 2 but false positive using concat() below
+          // push() builds the correct array no more false positive
+          count.push([ 1, S[i] ]);
+      }
+  }
+
+  // sum to track to k
+  let sum = 0;
+  // left index
+  let lpointer = 0;
+  // right index
+  let rpointer = 1;
+  // difference between l and r
+  let diff = 0;
+
+  // iterate through count
+  while (rpointer < count.lenth) {
+      // adds number of any size length
+      sum += count[rpointer][0].toString().length;
+      // adds for the letter if count greater than 1
+      sum += count[rpointer][0] > 1 ? 1 : 0;
+
+      // if sum over k busts
+      if (sum > k) {
+          // move left pointer
+          lpointer++;
+          // reset right pointer
+          rpointer = lpointer + 1;
+          // reset sum
+          sum = lpointer[0].toString().length;
+          sum += lpointer[0] > 1 ? 1 : 0;
+      }
+      // if sum is the same as k
+      if (sum === k) {
+          // check if l and r pointers have bigger difference than before
+         if (diff < rpointer - lpointer) {
+             // set diff to bigger diff
+             diff = rpointer - lpointer;
+             // reset l and r pointers
+             lpointer++;
+             rpointer = lpointer + 1;
+             // reset sum
+             sum = lpointer[0].toString().length;
+             sum += lpointer[0] > 1 ? 1 : 0;
+         }
+      }
+  }
+
+  // return counter to non-nested array
+  const consolidated = [];
+  for (let i = 0; i < count.length; i++) {
+      if (count[i][0] > 1) {
+          consolidated.push(count[i][0], count[i][1]);
+      } else {
+          consolidated.push(count[i][1]);
+      }
+  }
+
+  // return to string because non nested to get proper length
+  const res = consolidated.join('');
+
+  // length minus greatest diff for min string length
+  return res.length - diff;
+}
---
+++ problems/jumpGame.js
@@ -0,0 +1,57 @@
+/*
+
+Jump Game
+
+Code prompt found at:
+
+Difficulty: Medium
+
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+Example 1:
+
+Input: [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum
+             jump length is 0, which makes it impossible to reach the last index.
+
+*/
+
+// this times out in Leet Code Testing
+export const bruteForceJumpGame = (nums) => {
+  let res = false;
+
+  const jump = (i) => {
+    if (i >= nums.length - 1) {
+      res = true;
+      return;
+    }
+
+    for (let j = 1; j <= nums[i]; j++) {
+      jump(i + j);
+    }
+  }
+
+  jump(0);
+
+  return res;
+}
+
+export const canJumpDPBottom = (nums) => {
+  let target = nums.length - 1;
+
+  for (let i = target; i >= 0; i--) {
+    if (i + nums[i] >= target) target = i;
+  }
+
+  return target === 0;
+};
