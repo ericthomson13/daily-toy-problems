714d37535cd9bff506152b155e6eacace0898fe4
et <eric.thomson13@gmail.com>
2020-05-07 09:42:51 -0600

    Cousin Binary Problem, Merge Interval Solution Partial

---
+++ problems/may2020/cousinsInBinaryTree.js
@@ -0,0 +1,62 @@
+/*
+
+Code prompt found at:
+https://leetcode.com/problems/cousins-in-binary-tree/
+Difficulty: Easy
+
+In a binary tree, the root node is at depth 0, and children of each depth k node are at depth k+1.
+
+Two nodes of a binary tree are cousins if they have the same depth, but have different parents.
+
+We are given the root of a binary tree with unique values, and the values x and y of two different nodes in the tree.
+
+Return true if and only if the nodes corresponding to the values x and y are cousins.
+
+
+
+Example 1:
+Input: root = [1,2,3,4], x = 4, y = 3
+Output: false
+
+Example 2:
+Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
+Output: true
+
+Example 3:
+Input: root = [1,2,3,null,4], x = 2, y = 3
+Output: false
+
+
+Note:
+The number of nodes in the tree will be between 2 and 100.
+Each node has a unique integer value from 1 to 100.
+
+*/
+
+const cousinsInTree = (root, x, y) => {
+  if (!x || !y) return false;
+  if (x === y) return false;
+  if (!root.left || !root.right) return false;
+
+  let depthX = 0;
+  let depthY = 0;
+
+  const depthFinder = (node, target, depth) => {
+    if (!node) return;
+
+    if (node.val === target) {
+      depth++;
+      return;
+    };
+
+    depthFinder(node.right);
+    depthFinder(node.left);
+  }
+
+  depthFinder(root, x, depthX);
+  depthFinder(root, y, depthY);
+
+  return depthX === depthY;
+};
+
+export default cousinsInTree;
---
+++ problems/may2020/mergeInterval.js
@@ -0,0 +1,52 @@
+/*
+
+Code prompt found at:
+https://leetcode.com/problems/merge-intervals/submissions/
+Difficulty: Medium
+
+Given a collection of intervals, merge all overlapping intervals.
+
+Example 1:
+Input: [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+Example 2:
+Input: [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+Example 3:
+Input: [[1, 4], [0, 4]]
+Output: [[0, 4]]
+
+Example 4:
+Input: [[1,4],[0,0]]
+Output: [[0,0],[1,4]]
+*/
+
+// TODO: get working with Example 4
+
+const merge = (intervals) => {
+  let i = 0;
+  let j = 1;
+
+  while (i < intervals.length - 1) {
+    if (intervals[i][1] >= intervals[j][0]) {
+      let min = Math.min(intervals[i][0], intervals[j][0]);
+      let max = Math.max(intervals[i][1], intervals[j][1]);
+
+      intervals[i] = [min, max];
+      intervals.splice(j, 1);
+    } else if (j < intervals.length - 1) {
+      j++;
+    } else {
+      i++;
+      j = i + 1;
+    }
+  }
+
+  return intervals;
+};
+
+export default merge;
