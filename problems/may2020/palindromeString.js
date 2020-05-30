dd51d54867aab87db6cab540469c816e98420ad0
et <eric.thomson13@gmail.com>
2020-05-30 16:22:27 -0600

    Second Solution and Testing for Palindrome String

--- problems/may2020/palindromeString.js
+++ problems/may2020/palindromeString.js
@@ -6,7 +6,7 @@ A palindrome is a string that is the same when read forwards as backwards.
 
 */
 
-const palindromeStr = (string) => {
+export const palindromeStr = (string) => {
   if (string.length < 2) return true;
   string = string.toLowerCase();
 
@@ -19,6 +19,7 @@ const palindromeStr = (string) => {
     ',': true,
     '.': true,
     '!': true,
+    ':': true,
   };
 
   while (start <= end) {
@@ -41,4 +42,21 @@ const palindromeStr = (string) => {
   return true;
 }
 
-export default palindromeStr;
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't' , 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+export const isPalindrome2 = (s) => {
+    const chars = new Set();
+    letters.forEach((letter) => chars.add(letter));
+
+    const arr = s.toLowerCase().split('').filter((char) => chars.has(char));
+    let start = 0;
+    let end = arr.length - 1;
+
+    while (start < end) {
+        if (arr[start] !== arr[end]) return false;
+        start++;
+        end--;
+    }
+
+    return true;
+};
--- tests/may2020/palindromeString.test.js
+++ tests/may2020/palindromeString.test.js
@@ -1,17 +1,35 @@
-import palindromeStr from '../../problems/may2020/palindromeString';
+import { palindromeStr, isPalindrome2, } from '../../problems/may2020/palindromeString';
 
-describe('Palindrome String Test', () => {
+const palindromes = ['anna', 'civic', 'kayak', 'level', 'madam', 'mom', 'noon', 'racecar', 'Red rum sir, is murder', "A man, a plan, a canal: Panama"];
+const notPalindromes = ['string', 'none', 'not', 'false'];
+const short = ['', '1'];
 
-  test.each(['', '1'])('Returns true when given a string with length of 0 or 1', (i) => {
+describe('Palindrome String Tests', () => {
+
+  test.each(short)('Returns true when given a string with length of 0 or 1', (i) => {
     expect(palindromeStr(i)).toBe(true);
   });
 
-  test.each(['string', 'none', 'not', 'false'])('Returns false when string is not a palindrome', (i) => {
+  test.each(notPalindromes)('Returns false when string is not a palindrome', (i) => {
     expect(palindromeStr(i)).toBe(false);
   });
 
-  test.each(['anna', 'civic', 'kayak', 'level', 'madam', 'mom', 'noon', 'racecar', 'Red rum sir, is murder'])('Returns true when string is a palindrome', (i) => {
+  test.each(palindromes)('Returns true when string is a palindrome', (i) => {
     expect(palindromeStr(i)).toBe(true);
   });
+});
 
+describe('Palindrome Exampe 2 Tests', () => {
+
+  test.each(short)('Returns true when given a string with length of 0 or 1', (i) => {
+    expect(isPalindrome2(i)).toBe(true);
+  });
+
+  test.each(notPalindromes)('Returns false when string is not a palindrome', (i) => {
+    expect(isPalindrome2(i)).toBe(false);
+  });
+
+  test.each(palindromes)('Returns true when string is a palindrome', (i) => {
+    expect(isPalindrome2(i)).toBe(true);
+  });
 });
